d17e6398b65e3a15f3fd0edd751630a60ce2022d
joo2017 <30431754+joo2017@users.noreply.github.com>
2025-09-13 10:03:22 +0900

    Delete config/routes.rb

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-# 复用calendar的路由模式
-Discourse::Application.routes.draw do
-  scope "/admin/plugins" do
-    get "/lottery" => "admin/plugins#index", constraints: StaffConstraint.new
-    get "/lottery/settings" => "admin/lottery#settings", constraints: StaffConstraint.new
-    get "/lottery/active" => "admin/lottery#active", constraints: StaffConstraint.new
-    put "/lottery/:id/cancel" => "admin/lottery#cancel", constraints: StaffConstraint.new
-  end
-end
